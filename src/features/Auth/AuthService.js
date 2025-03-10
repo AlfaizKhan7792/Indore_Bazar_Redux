60a733913ba0c61c8b58c9d330e9c59b027c6ff2
AlfaizKhan7792 <alfaizkhan7792@gmail.com>
2025-03-10 16:51:49 +0530

    then

--- src/features/Auth/AuthService.js
+++ src/features/Auth/AuthService.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const API_URL = "https://authentication-api-qtax.onrender.com/api/user";
+// const API_URL = "https://authentication-api-qtax.onrender.com/api/user";
 
 export const Login = async (formData) =>{
     try {
-      const response = await axios.post(API_URL + "/login", formData);
-      // const response = await axios.post("/api/user/login", formData);
+      // const response = await axios.post(API_URL + "/login", formData);
+      const response = await axios.post("/api/user/login", formData);
     localStorage.setItem("Auth", JSON.stringify(response.data))
     return response.data
     } catch (error) {
@@ -15,8 +15,8 @@ export const Login = async (formData) =>{
 
 export const Register = async (formData) =>{
   try {
-    const response = await axios.post(API_URL + "/register", formData);
-    // const response = await axios.post("/api/user/register", formData);
+    // const response = await axios.post(API_URL + "/register", formData);
+    const response = await axios.post("/api/user/register", formData);
     localStorage.setItem("Auth", JSON.stringify(response.data))
     return response.data
   } catch (error) {
